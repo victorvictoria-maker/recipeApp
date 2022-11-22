5f90aae2621d320ac3e12afb4fba8bd25776a42d
victorvictoria-maker <viktohrier01@gmail.com>
2022-11-22 22:41:39 +0100

    Added oil recipe

--- src/store/index.js
+++ src/store/index.js
@@ -4,37 +4,37 @@ export default createStore({
   state: {
     recipes: [
       {
-        slug: 'katsu-curry',
-        title: 'Katsu-curry',
-        description: 'A delicious curry made with chicken, potatoes, and a special sauce',
+        slug: 'banana conditioner',
+        title: 'Banana conditioner',
+        description: 'A DIY conditioner made with banana for growth and hair retention',
         ingredients: [
-          '1 tablespoon olive oil',
-          '1 onion, chopped',
-          '2 cloves garlic, chopped',
-          '1 tablepoon curry powder'
+          '4 tablespoons of extra virgin coconut oil',
+          '2 tablespoons of  honey',
+          '2 tablespoons of  glycerin',
+          '1 large banana'
         ],
         method: [
-          'Heat oil in a large skillet over medium heat.',
-          'Add onion and garlic and cook, stirring often, until softened, about 5 minutes.',
-          'Add curry powder and cook, stirring, until fragrant, about 1 minute',
-          'Add curry powder and cook, stirring, until fragrant, about 1 minute'
+          'Slice the banana and place in a blender.',
+          'Add the glycerin, honey and virgin coconut oil.',
+          'Process for 30 minutes to one minute',
+          'Transfer the conditioner into a bowl'
         ]
       },
       {
-        slug: 'ramen soup',
-        title: 'Ramen soup',
-        description: 'A delicious curry made with chicken, potatoes, and a special sauce',
+        slug: 'avocado conditioner',
+        title: 'Avocado conditioner',
+        description: 'A DIY conditioner made with avocado for growth and hair retention',
         ingredients: [
-          '1 tablespoon olive oil',
-          '1 onion, chopped',
-          '2 cloves garlic, chopped',
-          '1 tablepoon curry powder'
+          '1/4 cup of extra virgin coconut oil',
+          '3 tablespoons of  apple cider vinegar',
+          '1/2 cup of shea butter',
+          '1 avocado'
         ],
         method: [
-          'Heat oil in a large skillet over medium heat.',
-          'Add onion and garlic and cook, stirring often, until softened, about 5 minutes.',
-          'Add curry powder and cook, stirring, until fragrant, about 1 minute',
-          'Add curry powder and cook, stirring, until fragrant, about 1 minute'
+          'Place the apple cider vinegar, shea butter, avocado and virgin coconut oil.',
+          'You do not have to melt the shea butter.',
+          'Process for one minute',
+          'Transfer the mixture into a bowl'
         ]
       }
     ]
